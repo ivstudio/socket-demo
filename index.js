80d33a80a7f868b0a8df3a28ecd129936ce53743
Ignacio Villamar <iggy@honeybook.com>
2018-01-13 17:05:59 -0500

    update port

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = 3000;
 
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+var portNumber = process.env.PORT || 5000;
+server.listen(portNumber);
+
 
 app.get('/', (req, res) =>{
    res.sendFile(__dirname + '/public/index.html');
